4deac80c7823527710022e204c151f8372af3428
Colten Jackson <jazzyjackson@gmail.com>
2018-02-27 18:03:59 -0500

    Create mixin.js

---
+++ mixin.js
@@ -0,0 +1,5 @@
+// edit this first line and call it whatever you want. overwrite document.createElement for all I car.
+// actually maybe that will be a nice upgrade. grab original createElement for safe keeping. if type is string, just call createElement. But I'll keep the two separate for now, and call mixint.createElement to be verbose that I'm doing something different
+window.mixin.createElement = function createElement(graph){
+
+}
