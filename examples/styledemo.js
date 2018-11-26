26a9b9c37abaf3ea7c34075017ab68eb59ee2049
Colten Jackson <jazzyjackson@gmail.com>
2018-11-26 06:00:07 -0600

    styledemo examples

---
+++ examples/styledemo.js
@@ -0,0 +1,16 @@
+const ctxify = require('../ctxify.node.js')
+const styledemo = require('./styledemo.ohtm.json')
+
+console.log(
+	ctxify(styledemo, {
+		data: {
+			innerdata: {
+				id: "somethingunique"
+			}
+		},
+		style: {
+			width: 444,
+			height: 222
+		}
+	})
+)
