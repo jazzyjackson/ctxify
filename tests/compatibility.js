01abc361ef4adf90df7dec0ad67c7e7e3c227af1
Colten Jackson <jazzyjackson@gmail.com>
2018-11-30 02:32:33 -0600

    added some optimistic test cases for the future, with examples in simpledemo (not so simple anymore)

---
+++ tests/compatibility.js
@@ -0,0 +1,14 @@
+TODO tests...it handles
+
+{{nested.data.from.context}}
+{{backet[notation][like][this]}}
+
+"a string file path to import structure"
+
+conditionals:
+	"{{#if some.data}}": {tagName: {attributes}}
+
+loops:
+	"{{#each some.data}}": {tagName: {attributes}} //<- via local context from array
+
+	{{{allows.unescaped.text}}}
