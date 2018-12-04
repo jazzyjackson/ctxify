41b014ae51cb106b4667c3eb6878300333cb34ab
Colten Jackson <jazzyjackson@gmail.com>
2018-12-04 04:09:45 -0600

    added gitignore and figjam dependency

--- tests/tutorial.js
+++ tests/tutorial.js
@@ -1,4 +1,4 @@
-const FigJam = require('../bin/figjam.js')
+const FigJam = require('@mixint/figjam')
 const BytePipette = require('@mixint/bytepipette')
 const http = require('http')
 const fs = require('fs')
@@ -7,8 +7,15 @@ http.createServer({
     IncomingMessage: require('parsedmessage'),
     ServerResponse: require('serverfailsoft'),
 }, (req, res) => ((route) => {
-	req.pipe(new route).pipe(res)
-	console.log(req.connection.remoteAddress, req.method, req.url)
+	console.log(
+		req.connection.remoteAddress,
+		req.method,
+		req.url
+	)
+	if(req.pathname == '/')
+		res.writeHead('302',{Location: '/about'}), res.end()
+	else
+		req.pipe(new route).pipe(res)
 })(
 	req.pathname.slice(-1) == '/' ? FigJam : BytePipette
 )).listen(process.env.PORT || 3000)
